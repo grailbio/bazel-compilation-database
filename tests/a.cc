91e5b590382c86825457155b2045b1c411efe0a8
Cherry Ng <cherry92@gmail.com>
2018-05-13 17:48:01 +0800

    Fix travis tests.

--- tests/a.cc
+++ tests/a.cc
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "a.h"
+#include "tests/a.h"
 
 int A::field_value() const {return field_a;}
--- tests/b.cc
+++ tests/b.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "a.h"
+#include "tests/a.h"
 
 class B : A {
  public:
