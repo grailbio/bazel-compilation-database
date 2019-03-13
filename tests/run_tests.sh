d6e0ef23861fe799515b8d09578b6d82b2c4499a
Siddhartha Bagaria <sbagaria@grailbio.com>
2019-03-12 18:57:28 -0700

    Simplify YCM extra conf test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,8 +7,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 bazel build :compdb
 diff expected_file.json bazel-bin/compile_commands.json
 
-ln -s ../aspects.bzl .
-cp ../.ycm_extra_conf.py .
-diff expected_ycm_output.json <(python .ycm_extra_conf.py a.cc)
-rm ./.ycm_extra_conf.py
-rm ./aspects.bzl
+diff expected_ycm_output.json <(python ../.ycm_extra_conf.py a.cc)
