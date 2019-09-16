aaac16070bc8b1c92a9e4887e7b396244944abcf
Siddhartha Bagaria <siddharthab@users.noreply.github.com>
2019-09-16 17:44:08 +0000

    Update bazelisk to v1.0

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,7 +9,7 @@ readonly os
 
 # Use bazelisk to catch migration problems.
 # Value of BAZELISK_GITHUB_TOKEN is set as a secret on Travis.
-readonly url="https://github.com/bazelbuild/bazelisk/releases/download/v0.0.8/bazelisk-${os}-amd64"
+readonly url="https://github.com/bazelbuild/bazelisk/releases/download/v1.0/bazelisk-${os}-amd64"
 readonly bin_dir="${TMPDIR:-/tmp}/bin"
 readonly bazel="${bin_dir}/bazel"
 
