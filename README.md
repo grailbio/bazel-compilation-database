20591c7ebc597faca2c441d60c025a8a6767349f
Siddhartha Bagaria <sbagaria@grailbio.com>
2019-10-30 00:29:33 -0700

    readlink substitute for macOS

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ somewhere in your PATH.
 For example,
 ```sh
 INSTALL_DIR="/usr/local/bin"
-VERSION="0.4.1"
+VERSION="0.4.2"
 
 # Download and symlink.
 (
--- generate.sh
+++ generate.sh
@@ -22,7 +22,48 @@
 
 set -e
 
-readonly ASPECTS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+# This function is copied from https://source.bazel.build/bazel/+/master:scripts/packages/bazel.sh.
+# `readlink -f` that works on OSX too.
+function get_realpath() {
+	if [ "$(uname -s)" == "Darwin" ]; then
+		local queue="$1"
+		if [[ "${queue}" != /* ]] ; then
+			# Make sure we start with an absolute path.
+			queue="${PWD}/${queue}"
+		fi
+		local current=""
+		while [ -n "${queue}" ]; do
+			# Removing a trailing /.
+			queue="${queue#/}"
+			# Pull the first path segment off of queue.
+			local segment="${queue%%/*}"
+			# If this is the last segment.
+			if [[ "${queue}" != */* ]] ; then
+				segment="${queue}"
+				queue=""
+			else
+				# Remove that first segment.
+				queue="${queue#*/}"
+			fi
+			local link="${current}/${segment}"
+			if [ -h "${link}" ] ; then
+				link="$(readlink "${link}")"
+				queue="${link}/${queue}"
+				if [[ "${link}" == /* ]] ; then
+					current=""
+				fi
+			else
+				current="${link}"
+			fi
+		done
+
+		echo "${current}"
+	else
+		readlink -f "$1"
+	fi
+}
+
+readonly ASPECTS_DIR="$(dirname "$(get_realpath "${BASH_SOURCE[0]}")")"
 readonly OUTPUT_GROUPS="compdb_files"
 
 readonly WORKSPACE="$(bazel info workspace)"
